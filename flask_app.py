208a4ce1021cd0e7a4be1892f0ec77c1c95f58c3
Robin Titus <143838819+titusrobin@users.noreply.github.com>
2024-07-16 22:22:48 -0400

    Updated Flask port to 5001

--- flask_app.py
+++ flask_app.py
@@ -9,5 +9,5 @@ def handle_webhook():
     return webhook()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
